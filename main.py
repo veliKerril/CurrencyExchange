be802b460a9a6472811dbe7264457eae48db021b
veliKerril <velikirilll@gmail.com>
2023-08-21 22:02:21 +0300

    v 2.1
    Попытка настроить под хостинг на сервер

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from controller import HTTPRequestHandler
 
 # Запускаем сервер, передаем IP и порт, по которому будем передавать информацию. Вторым аргументом
 # вносим класс, который отвечает за обработку http запросов.
-server = server.HTTPServer(('localhost', 8000), HTTPRequestHandler)
+server = server.HTTPServer(('87.236.19.5', 8888), HTTPRequestHandler)
 server.serve_forever()
